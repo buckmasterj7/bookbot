b99e3f827fc02979f8a6b6efad1a4a0a22d56d4f
buckmasterj7 <jlbuckmaster7@gmail.com>
2025-07-01 21:12:00 -0700

    add book text function

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents    
+    
+from stats import get_word_count
+from stats import get_character_count
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    word_count = get_word_count(book_text)
+    character_count = get_character_count(book_text)
+    print(f"{word_count} words found in the document")
+    print(character_count)
 
 main()
