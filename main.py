b9161326306d64525b4d6e6c8eec2e593d516cc2
buckmasterj7 <jlbuckmaster7@gmail.com>
2025-07-08 15:57:54 -0700

    added report print function

--- main.py
+++ main.py
@@ -5,12 +5,22 @@ def get_book_text(path_to_file):
     
 from stats import get_word_count
 from stats import get_character_count
+from stats import get_sorted_character_list
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     word_count = get_word_count(book_text)
     character_count = get_character_count(book_text)
-    print(f"{word_count} words found in the document")
-    print(character_count)
+    characters = get_sorted_character_list(character_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char in characters:
+        if char["char"].isalpha():
+            print(f"{char['char']}: {char['num']}")
+    print("============= END ===============")
 
 main()
+
--- stats.py
+++ stats.py
@@ -12,3 +12,20 @@ def get_character_count(book_text):
         else:
             character_count[text] = 1
     return character_count
+
+def sort_on(items):
+    return items["num"]
+
+def get_sorted_character_list(character_count):
+    characters = []
+    for character in character_count:
+        num = character_count[character]
+        char_dict = {
+            "char": character,
+            "num" : num
+        }
+        characters.append(char_dict)
+    characters.sort(reverse=True, key=sort_on)
+    return characters
+
+
