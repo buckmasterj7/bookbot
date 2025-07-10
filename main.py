ab7fa8277c333d14ce9e94f1fc69b77a4eeb287e
buckmasterj7 <jlbuckmaster7@gmail.com>
2025-07-09 20:17:15 -0700

    added sys module and more books

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
@@ -8,12 +10,19 @@ from stats import get_character_count
 from stats import get_sorted_character_list
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path_to_file = sys.argv[1]
+    book_text = get_book_text(path_to_file)
     word_count = get_word_count(book_text)
     character_count = get_character_count(book_text)
     characters = get_sorted_character_list(character_count)
+
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_file}")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
